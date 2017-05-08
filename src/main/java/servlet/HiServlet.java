b48137102e4a23e56b6faf8c36fa8526c0efdde7
Kohányi Róbert <kohanyi.robert@gmail.com>
2017-05-08 19:21:04 +0000

    Remove MariaDB4J
    * Added Spring ScriptUtils to execute init.sql
    * Move classes inside packages
    * Removed web.xml - there was no need for it

--- src/main/java/servlet/HiServlet.java
+++ src/main/java/servlet/HiServlet.java
@@ -1,26 +1,33 @@
+package servlet;
+
+import dao.DatabaseHiDao;
+import dao.HiDao;
+import util.ConnectionUtil;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.SQLException;
+import java.sql.Connection;
 
 @WebServlet("/hi")
 public class HiServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try {
-            String name = req.getParameter("name");
-            String lang = req.getParameter("lang");
+        String name = req.getParameter("name");
+        String lang = req.getParameter("lang");
 
-            String msg = MessageDAO.getMsg(lang);
+        try (Connection conn = ConnectionUtil.getConnection(ConnectionUtil.DatabaseName.BFA)) {
+            HiDao dao = new DatabaseHiDao(conn);
+            String msg = dao.getMsg(lang);
             req.setAttribute("msg", String.format(msg, name));
-
-            req.getRequestDispatcher("name.jsp").forward(req, resp);
-        } catch (SQLException e) {
+        } catch (Exception e) {
             throw new ServletException(e);
         }
+
+        req.getRequestDispatcher("name.jsp").forward(req, resp);
     }
 }
---
+++ src/main/java/util/ConnectionUtil.java
@@ -0,0 +1,47 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public final class ConnectionUtil {
+
+    public enum DatabaseName {
+
+        TEST,
+        BFA
+    }
+
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DATABASE_HOST = "localhost";
+    private static final String DATABASE_USER = "root";
+    private static final String DATABASE_PASSWORD = "";
+    private static final int DATABASE_PORT = 3306;
+
+    static {
+        try {
+            Class.forName(DRIVER).newInstance();
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public static String getUrl(DatabaseName databaseName) {
+        return String.format(
+            "jdbc:mysql://%s:%s/%s?useUnicode=true&characterEncoding=UTF-8",
+            DATABASE_HOST,
+            DATABASE_PORT,
+            databaseName.name().toLowerCase());
+    }
+
+    public static Connection getConnection(DatabaseName databaseName) {
+        try {
+            return DriverManager.getConnection(getUrl(databaseName), DATABASE_USER, DATABASE_PASSWORD);
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private ConnectionUtil() {
+    }
+}
